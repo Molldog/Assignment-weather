451b9bb52e1509f3fcd0493ed7d843900898c899
Mollie <m.glaves@yahoo.com.au>
2023-05-29 17:12:37 +1000

    Updated css all colours

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function displayWeather(response) {
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}02x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
