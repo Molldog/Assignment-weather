0775bde3a2880d5c2307fef0b2fc104702f15e76
Mollie <m.glaves@yahoo.com.au>
2023-06-03 14:39:31 +1000

    Updated html and js
    html - removed class from col-12
    js - created formatDay function, forecast variables and current index, linked icons and weather data using api

--- script.js
+++ script.js
@@ -24,31 +24,47 @@ function formatDate(timestamp) {
   return `${day} | ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
-
   let forecastHTML = `<div class="row>`;
-
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
        <div class="col-2">
-           <ul>
-             <li class="day">${day}</li>
-             <li class="emoji">🌤</li>
-             <li class="high-temp">
-                 28° <span class="low-temp"> 23° </span>
-             </li>
-             <li class="rain">💧 5% 1mm</li>
-            <li class="humidity">humidity</li>
-             <li class="wind">💨 4km/hr</li>
-           </ul>
+             <div class="weather-forecast-date">${formatDay(
+               forecastDay.dt
+             )}</div>
+             <img
+             src="http://openweathermap.org.img.wn/${
+               forecastDay.weather[0].icon
+             }@2x.png"
+             alt="weather-icon"
+             width="42"
+             />
+             <div class="high-temp">
+                 ${Math.round(
+                   forecastDay.temp.max
+                 )}° <span class="low-temp"> ${Math.round(
+          forecastDay.temp.min
+        )}° </span>
+             </div>
+             <li class="rain">${forecastDay.rain}</li>
+            <li class="humidity">${forecastDay.humidity}</li>
+             <li class="wind">${forecastDay.wind}km/hr</li>
        </div>
    `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -57,7 +73,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "9ff62228143f9cc1758df8baa86a6b09";
-  let apiUrl = `https://api.openweathermao.org/data/2.5/onecall?
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
   lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -83,6 +99,8 @@ function displayWeather(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   celsiusTemperature = response.data.main.temp;
+
+  let temperatureElement = document.querySelector("#celsius-link");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 
   getForecast(response.data.coords);
